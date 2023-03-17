2c4ea0d97355c2f9849d6ef6ff39b6dc1763142b
Stobbart <adam83smith@gmail.com>
2023-03-17 14:39:13 +0000

    initial xcode files

---
+++ iOSTestFlightGithubActions/iOSTestFlightGithubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestFlightGithubActionsApp.swift
+//  iOSTestFlightGithubActions
+//
+//  Created by Adam Smith on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSTestFlightGithubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
